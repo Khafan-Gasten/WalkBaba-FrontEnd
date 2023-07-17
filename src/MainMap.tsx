e3982f4e3906a29c62aa2272812ec1eed63eb95f
Max Burrows <maxajburrows@gmail.com>
2023-07-17 18:15:56 +0200

    Move map to separate component

---
+++ src/MainMap.tsx
@@ -0,0 +1,86 @@
+import React, {useRef, useState, useEffect} from 'react'
+
+import {
+    GoogleMap,
+    useJsApiLoader,
+    DirectionsRenderer,
+    Marker,
+    Autocomplete,
+    DirectionsService
+} from '@react-google-maps/api';
+
+
+const containerStyle = {
+    width: '400px',
+    height: '400px'
+};
+
+const center = {
+    lat: 52.2,
+    lng: 4.4
+};
+const origin = "Amsterdam, Netherlands"
+
+const dest = "Rotterdam, Netherlands"
+
+function MainMap() {
+    const {isLoaded} = useJsApiLoader({
+        id: 'google-map-script',
+        googleMapsApiKey: "AIzaSyBmOpstO2144GQzwOWrWL9NQLvQ5oyE_kw"
+    });
+    const [map, setMap] = useState<google.maps.Map | null>(null);
+    const [directionsResponse, setDirectionsResponse] = useState<google.maps.DirectionsResult | null>(null);
+    const originRef = useRef();
+    const destinationRef = useRef();
+
+
+
+    const onLoad = React.useCallback(function callback(map) {
+        calculateRoute()
+        console.log("loading")
+        setMap(map)
+    }, [])
+// useEffect( () => {
+//     // setMap(null)
+//     console.log("use effect called")
+//     calculateRoute()
+// }, [])
+// const onUnmount = React.useCallback(function callback() {
+//     // setMap(null)
+//       calculateRoute()
+// }, [])
+
+    async function calculateRoute() {
+        console.log(window)
+        // eslint-disable-next-line no-undefined
+
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+        console.log("In calculate route")
+        const directionsService = new window.google.maps.DirectionsService();
+        const results: google.maps.DirectionsResult = await directionsService.route({
+            origin: origin,
+            destination: dest,
+            travelMode: google.maps.TravelMode.DRIVING
+        })
+        console.log(results)
+        setDirectionsResponse(results)
+        // setDistance(results.routes[0].legs[0].distance.text)
+        // setDuration(results.routes[0].legs[0].duration.text)
+    }
+
+    return isLoaded ? (
+            <GoogleMap
+                mapContainerStyle={containerStyle}
+                center={center}
+                zoom={8}
+                onLoad={onLoad}
+                // onUnmount={onUnmount}
+            >
+                {directionsResponse && (
+                    <DirectionsRenderer directions={directionsResponse}/>
+                )}
+            </GoogleMap>
+    ) : <></>
+
+}
+export default MainMap
