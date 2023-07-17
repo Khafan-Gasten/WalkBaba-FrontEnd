5261b39baff5b7f14a6bb357c483206fa3e81fa1
Saghi Paknemat <paknemat@gmail.com>
2023-07-17 12:08:35 +0200

    Ignore .env file

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,52 @@
-import React, { useState } from 'react'
+import React, {useState} from 'react'
 import './App.css'
-import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
+import {GoogleMap, useJsApiLoader} from '@react-google-maps/api';
+
 const containerStyle = {
     width: '400px',
     height: '400px'
 };
 
 const center = {
-    lat: -3.745,
-    lng: -38.523
+    lat: 52.2,
+    lng: 4.4
 };
+
 function App() {
 
+    const {isLoaded} = useJsApiLoader({
+        id: 'google-map-script',
+        googleMapsApiKey: process.env.REACT_APP_MAPS_KEY ? process.env.REACT_APP_MAPS_KEY : "Maboo",
+    })
 
-        const { isLoaded } = useJsApiLoader({
-            id: 'google-map-script',
-            googleMapsApiKey: ""
-        })
+    const [map, setMap] = React.useState(null)
 
-        const [map, setMap] = React.useState(null)
+    const onLoad = React.useCallback(function callback(map) {
+        const bounds = new window.google.maps.LatLngBounds(center);
+        map.fitBounds(bounds);
+        console.log("loading")
 
-        const onLoad = React.useCallback(function callback(map) {
-            // This is just an example of getting and using the map instance!!! don't just blindly copy!
-            const bounds = new window.google.maps.LatLngBounds(center);
-            map.fitBounds(bounds);
 
-            setMap(map)
-        }, [])
+        setMap(map)
+    }, [])
 
-        const onUnmount = React.useCallback(function callback(map) {
-            setMap(null)
-        }, [])
+    const onUnmount = React.useCallback(function callback(map) {
+        setMap(null)
+    }, [])
 
-        return isLoaded ? (
-            <GoogleMap
-                mapContainerStyle={containerStyle}
-                center={center}
-                zoom={10}
-                onLoad={onLoad}
-                onUnmount={onUnmount}
-            >
+    return isLoaded ? (
+        <GoogleMap
+            mapContainerStyle={containerStyle}
+            center={center}
+            zoom={8}
+            onLoad={onLoad}
+            onUnmount={onUnmount}
+        >
+        </GoogleMap>
+    ) : <>
 
-            </GoogleMap>
-        ) : <></>
-    }
-
+        <p>jhsdafdkakh</p></>
+}
 
 
 export default App
