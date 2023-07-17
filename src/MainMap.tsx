27428a3339d195a1ce88d7d3155ace7a03b482eb
Max Burrows <maxajburrows@gmail.com>
2023-07-17 18:18:19 +0200

    Tidy code

--- src/MainMap.tsx
+++ src/MainMap.tsx
@@ -1,12 +1,9 @@
-import React, {useRef, useState, useEffect} from 'react'
+import React, {useRef, useState} from 'react'
 
 import {
     GoogleMap,
     useJsApiLoader,
     DirectionsRenderer,
-    Marker,
-    Autocomplete,
-    DirectionsService
 } from '@react-google-maps/api';
 
 
@@ -30,39 +27,20 @@ function MainMap() {
     });
     const [map, setMap] = useState<google.maps.Map | null>(null);
     const [directionsResponse, setDirectionsResponse] = useState<google.maps.DirectionsResult | null>(null);
-    const originRef = useRef();
-    const destinationRef = useRef();
-
-
 
     const onLoad = React.useCallback(function callback(map) {
         calculateRoute()
         console.log("loading")
         setMap(map)
     }, [])
-// useEffect( () => {
-//     // setMap(null)
-//     console.log("use effect called")
-//     calculateRoute()
-// }, [])
-// const onUnmount = React.useCallback(function callback() {
-//     // setMap(null)
-//       calculateRoute()
-// }, [])
 
     async function calculateRoute() {
-        console.log(window)
-        // eslint-disable-next-line no-undefined
-
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-        console.log("In calculate route")
         const directionsService = new window.google.maps.DirectionsService();
         const results: google.maps.DirectionsResult = await directionsService.route({
             origin: origin,
             destination: dest,
             travelMode: google.maps.TravelMode.DRIVING
         })
-        console.log(results)
         setDirectionsResponse(results)
         // setDistance(results.routes[0].legs[0].distance.text)
         // setDuration(results.routes[0].legs[0].duration.text)
@@ -74,13 +52,11 @@ function MainMap() {
                 center={center}
                 zoom={8}
                 onLoad={onLoad}
-                // onUnmount={onUnmount}
             >
                 {directionsResponse && (
                     <DirectionsRenderer directions={directionsResponse}/>
                 )}
             </GoogleMap>
     ) : <></>
-
 }
 export default MainMap
