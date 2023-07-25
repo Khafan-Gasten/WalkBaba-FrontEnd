3b0f1dd94efa03df6b32d411d881b49831513fe7
Max Burrows <maxajburrows@gmail.com>
2023-07-25 23:18:15 +0200

    Write loading messages

--- src/LoadingPage.tsx
+++ src/LoadingPage.tsx
@@ -1,21 +1,24 @@
 import "./App.css";
 import "./LoadPage.css"
 import Baba from "./assets/com-gif-maker-unscreen.gif"
-import Baba2 from "./assets/ezgif.com-gif-maker-nb.gif"
 
 type LoadingPageProps = {
     displayMap: boolean
 }
 
 function LoadingPage(props: LoadingPageProps) {
-    const loadingMessage1 = "Great"
+    const loadingMessage1 = "We are scouring the internet to find the very best land marks just for you! The internet's pretty big, this may take a minute!"
+    const loadingMessage2 = "Did you know WalkBaba gives 80% of all profits to charity?! What a day you're having! Planning a fantastic walk and saving lives - sight seeing has never been so productive!"
+    const loadingMessage3 = "Now using our cutting edge AI to optimize the routes until they are perfect! Only the best for a WalkBaba babe."
+    const loadingMessage4 = "Did you know WalkBaba was founded in 2023 by Mahboubeh Sheidaei, Saghi Paknemat, Cornelis Verkade and Max Burrows, emphatically illustrating that Iran and Western Europe can get along."
+    const loadingMessage5 = "Just like Baba below we are almost there! Give us a couple more seconds I promise you won't be dissapointed!"
+    const loadingMessages: string[] = [loadingMessage1, loadingMessage2, loadingMessage3, loadingMessage4, loadingMessage5]
     return (
         <>
             <main>
                 <p>Hello, hello I rendered the loading page</p>
                 <div className="gif-container">
                     <img src={Baba} className="frame"/>
-                    <img src={Baba2} className="frame2"/>
                     {/*<iframe src="assets/ezgif.com-gif-maker.gif" width="100%" height="100%"*/}
                     {/*        frameBorder="0" className="giphy-embed frame-end" allowFullScreen></iframe>*/}
                 </div>
