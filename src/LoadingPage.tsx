3cd5a5293ce4397be5b0547bdc4f8454314d4da5
Max Burrows <maxajburrows@gmail.com>
2023-07-25 22:59:55 +0200

    Mostly remove Baba's background

--- src/LoadingPage.tsx
+++ src/LoadingPage.tsx
@@ -1,18 +1,21 @@
 import "./App.css";
 import "./LoadPage.css"
-import Baba from "./assets/ezgif.com-gif-maker.gif"
+import Baba from "./assets/com-gif-maker-unscreen.gif"
+import Baba2 from "./assets/ezgif.com-gif-maker-nb.gif"
 
 type LoadingPageProps = {
     displayMap: boolean
 }
 
 function LoadingPage(props: LoadingPageProps) {
+    const loadingMessage1 = "Great"
     return (
         <>
             <main>
                 <p>Hello, hello I rendered the loading page</p>
                 <div className="gif-container">
                     <img src={Baba} className="frame"/>
+                    <img src={Baba2} className="frame2"/>
                     {/*<iframe src="assets/ezgif.com-gif-maker.gif" width="100%" height="100%"*/}
                     {/*        frameBorder="0" className="giphy-embed frame-end" allowFullScreen></iframe>*/}
                 </div>
